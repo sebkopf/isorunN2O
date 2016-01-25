c4b1182b31c227bc4772104a5cf0ebdfc1ae25b3
Sebastian Kopf <seb.kopf@gmail.com>
2016-01-25 17:19:14 -0500

    import extract instead of expand

--- R/package.R
+++ R/package.R
@@ -15,7 +15,7 @@
 #' @include calculations.R
 #' @include plotting.R
 #' @import dplyr
-#' @importFrom tidyr gather gather_ expand expand_ spread spread_
+#' @importFrom tidyr gather gather_ extract extract_ spread spread_
 #' @import lazyeval
 #' @import ggplot2
 NULL
