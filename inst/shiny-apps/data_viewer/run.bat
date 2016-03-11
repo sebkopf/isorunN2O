1e7507083767d7d8696f4266ccb60b32543697e8
Sebastian Kopf <seb.kopf@gmail.com>
2016-03-11 17:14:27 -0500

    version fix

--- inst/shiny-apps/data_viewer/run.bat
+++ inst/shiny-apps/data_viewer/run.bat
@@ -1,3 +1,3 @@
 Echo Launch dir: "%~dp0"
 Echo Current dir: "%CD%"
-"C:\Program Files\R\R-3.0.1\bin\Rscript.exe" "library(isorunN2O); run_data_viewer(launch.browser = TRUE)"
+"C:\Program Files\R\R-3.2.4\bin\Rscript.exe" "library(isorunN2O); run_data_viewer(launch.browser = TRUE)"
