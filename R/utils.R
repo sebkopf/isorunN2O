0b4726ff0b167c23e6d409125dd6f6e528904fbb
Sebastian Kopf <seb.kopf@gmail.com>
2016-05-31 22:38:41 -0400

    fix round to err function

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,8 @@
 #' error round
-round_to_err <- function(x, sd, sig_digits = 1) {
+round_to_err <- function(x, sd, sig_digits = 1, sep = "+-") {
   stopifnot(is.numeric(x))
   stopifnot(length(x) == length(sd))
-  n_deci <- nchar(sub('^[0-9]*\\.([0-9]*)0*$', '\\1', as.character(x)))
+  n_deci <- nchar(sub('^[0-9]*\\.([0-9]*)0*$', '\\1', as.character(signif(sd, sig_digits))))
   n_deci[(x %% 1) == 0] <- 0 # numbers w/o any decimal places
-  n_deci <- get_n_decimals(signif(sd, sig_digits))
-  paste0(round(x, n_deci), "+-", round(sd, n_deci))
+  paste0(round(x, n_deci), sep, round(sd, n_deci))
 }
