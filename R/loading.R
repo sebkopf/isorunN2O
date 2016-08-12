bd044e2f2b2c795860589042f908b778fc2c5600
Sebastian Kopf <seb.kopf@gmail.com>
2016-08-12 02:16:50 -0600

    loading functioanltiy improved (easier caching)

--- R/loading.R
+++ R/loading.R
@@ -3,11 +3,11 @@
 #' This function loads all the raw data files and caches the raw data if \code{cache} is set.
 #'
 #' @param folder the folder that contains the run data files (for now only isodat .dxf files are supported, functionality is provided by the \link{isoread} package).
-#' @param cache a cache folder if set (can be "." for current folder), caches the raw data at the specified location, by default set to "cache", set to NULL to avoid caching
+#' @param cache a cache folder if set (can be "." for current folder), caches the raw data at the specified location, by default set to the folder where the data is, set to NULL to avoid caching
 #' @param quiet whether the function should output information messages or be quiet (default is to output)
 #' @param ... parameters passed on to \code{\link{isoread}}
 #' @export
-load_run_folder <- function(folder, cache = "cache", quiet = F, ...) {
+load_run_folder <- function(folder, cache = folder, quiet = F, ...) {
   if (!dir.exists(folder))
     stop("The provided data folder '", folder,
          "' does not seem to exist (current working directory: ", getwd(), ")")
@@ -15,19 +15,37 @@ load_run_folder <- function(folder, cache = "cache", quiet = F, ...) {
   if (!is.null(cache) && !dir.exists(cache))
     dir.create(cache)
 
-  cache_file <- file.path(cache, make.names(basename(folder)) %>% paste0(".RData"))
+  cache_file <- default_cache_file(cache, basename(folder))
   if (!is.null(cache) && file.exists(cache_file)) {
-    if (!quiet) message("Loading data from cached file at ", cache_file)
-    load(cache_file, env = .GlobalEnv)
+    raw_data <- load_cache(cache_file, quiet = quiet)
   } else {
     if (!quiet) message("Loading data from isodat files in ", folder)
     raw_data <-
       isoread::isoread_folder(folder, type = "CFLOW", ext = ".dxf", quiet = quiet, ...)
     if (!is.null(cache)) {
-      if (!quiet) message("Caching data at ", cache_file)
-      save(raw_data, file = cache_file)
+      save_cache(raw_data, cache_file, quiet = quiet)
     }
   }
 
   return(raw_data)
 }
+
+#' default cache file name
+default_cache_file <- function(path, name) {
+  file.path(path, make.names(name) %>% paste0(".RData"))
+}
+
+#' store cache file
+save_cache <- function(data, cache_file, quiet = F) {
+  cache <- data
+  if (!quiet) message("Caching data at ", cache_file)
+  save(cache, file = cache_file)
+}
+
+#' store cach file
+load_cache <- function(cache_file, quiet = F) {
+  if (!quiet) message("Loading data from cached file at ", cache_file)
+  load(cache_file)
+  return(cache)
+}
+
